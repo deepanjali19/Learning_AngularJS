9b06dccfb6ac18fa591101c993a19474fd8a58a7
Deepanjali Gerangal <deepanjali.g@yahoo.com>
2019-08-02 20:57:57 -0400

    practicing basics

---
+++ WebApplication2/Scripts/script.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="angular.min.js" />
+
+var myApp = angular
+    .module("myModule", [])
+    .controller("myController", function ($scope) {
+
+        var employee = [
+            { first: "kuchi", last: "gerangal", gender: "female", salary: "70000" },
+            { first: "vivek", last: "gerangal", gender: "male", salary: "70000" },
+            { first: "amrit", last: "singh", gender: "male", salary: "90000" },
+            { first: "surinder", last: "gerangal", gender: "female", salary: "70000" },
+            { first: "chander vikas", last: "gerangal", gender: "male", salary: "80000" },
+        ];
+
+        $scope.employee = employee;
+    });
