7b84ab29d7775f32fa6043ed039322ac5b821dbe
Deepanjali Gerangal <deepanjali.g@yahoo.com>
2019-08-02 21:18:25 -0400

    updating variables

--- WebApplication2/Scripts/script.js
+++ WebApplication2/Scripts/script.js
@@ -1,15 +1,15 @@
-﻿/// <reference path="angular.min.js" />
+﻿ /// <reference path="angular.min.js" />
 
 var myApp = angular
     .module("myModule", [])
     .controller("myController", function ($scope) {
 
         var employee = [
-            { first: "kuchi", last: "gerangal", gender: "female", salary: "70000" },
-            { first: "vivek", last: "gerangal", gender: "male", salary: "70000" },
-            { first: "amrit", last: "singh", gender: "male", salary: "90000" },
-            { first: "surinder", last: "gerangal", gender: "female", salary: "70000" },
-            { first: "chander vikas", last: "gerangal", gender: "male", salary: "80000" },
+            { firstName: "Captain", lastName: "America", gender: "Male", salary: "70000" },
+            { firstName: "Iron", lastName: "Man", gender: "Male", salary: "70000" },
+            { firstName: "Doctor", lastName: "Strange", gender: "Male", salary: "90000" },
+            { firstName: "Super", lastName: "Man", gender: "Male", salary: "70000" },
+            { firstName: "Black", lastName: "Widow", gender: "Female", salary: "80000" },
         ];
 
         $scope.employee = employee;
