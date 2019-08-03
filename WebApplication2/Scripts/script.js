186c81885411ec39e1b97b261d7643727fd9a133
Deepanjali Gerangal <deepanjali.g@yahoo.com>
2019-08-03 15:10:31 -0400

    code for handling events

---
+++ WebApplication2/Scripts/script.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="angular.min.js" />
+
+var myApp = angular
+    .module("myModule", [])
+    .controller("myController", function ($scope) {
+
+        var technologies = [
+            { name: "C++", likes: 0, dislikes: 0 },
+            { name: "Java", likes: 0, dislikes: 0 },
+            { name: "AngulasJS", likes: 0, dislikes: 0 },
+            { name: "Prolog", likes: 0, dislikes: 0 },
+        ];
+
+        $scope.technologies = technologies;
+
+        $scope.incrementLikes = function (technology) {
+            technology.likes++;
+        } 
+
+        $scope.incrementDislikes = function (technology) {
+            technology.dislikes++;
+        } 
+    });
