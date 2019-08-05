0adc9ca115d1fd7801fe1e0cc2030c16a34e1b6c
Deepanjali Gerangal <deepanjali.g@yahoo.com>
2019-08-05 14:48:09 -0400

    initial code

--- WebApplication2/Scripts/script.js
+++ WebApplication2/Scripts/script.js
@@ -3,32 +3,5 @@
 var myApp = angular
     .module("myModule", [])
     .controller("myController", function ($scope) {
-        var countries = [
-            {
-                name: "India",
-                cities: [
-                    { name: "Delhi" },
-                    { name: "Bangalore" },
-                    { name: "Mumbai" },
-                ]
-            },
-            {
-                name: "Europe",
-                cities: [
-                    { name: "Luxembourg" },
-                    { name: "Switzerland" },
-                    { name: "Paris" },
-                ]
-            },
-            {
-                name: "USA",
-                cities: [
-                    { name: "California" },
-                    { name: "New York" },
-                    { name: "Chicago" },
-                ]
-            }
-        ];
-
-        $scope.countries = countries;
+      
     });
